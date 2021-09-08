e4678f9de9f9e0d5221b309860ff7b64be53d22f
jslopeza <jasonlopeza95@gmail.com>
2021-09-08 14:46:11 -0400

    Use a tag with disabled lint rule

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -17,17 +16,13 @@ export default function Home() {
         </h1>
 
         <div className={styles.grid}>
-          <Link href="/">
-            <a className={styles.card}>
-              <h2>Home</h2>
-            </a>
-          </Link>
+          <a href="/" className={styles.card}>
+            <h2>Home</h2>
+          </a>
 
-          <Link href="/app-a">
-            <a className={styles.card}>
-              <h2>App A</h2>
-            </a>
-          </Link>
+          <a href="/app-a" className={styles.card}>
+            <h2>App A</h2>
+          </a>
         </div>
       </main>
     </div>
