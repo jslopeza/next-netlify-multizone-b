f6c527dd7aaab26b043f9d64d3e21947eafd998f
jslopeza <jasonlopeza95@gmail.com>
2021-09-08 12:01:35 -0400

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">App B</a>
         </h1>
 
         <p className={styles.description}>
